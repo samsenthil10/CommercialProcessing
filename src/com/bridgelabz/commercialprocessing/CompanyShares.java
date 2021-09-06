85df4b5c41e2c3fbcc4ef1cc04d0ceec2d3661ae
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-06 05:57:52 +0530

    Added: To store details regarding company shares

---
+++ src/com/bridgelabz/commercialprocessing/CompanyShares.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.commercialprocessing;
+
+public class CompanyShares {
+	
+	private String name;
+	private int numberOfShares;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfShares() {
+		return numberOfShares;
+	}
+
+	public void setNumberOfShares(int numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+
+}
